0150aabf304a8634a281d4db770731cfadbbd561
Fabricio-Medina <patricmb2012@gmail.com>
2026-07-27 14:24:44 -0600

    Update 05_Creacion_de_cuenta.py

--- 05_Creacion_de_cuenta.py
+++ 05_Creacion_de_cuenta.py
@@ -8,6 +8,7 @@ nombreUsuario = input ("Introduce un nombre de usuario: ")
 contraseña = input ("Crea una contraseña (puede contener letras y numeros): ")
 if nombreUsuario == contraseña:
     print ("Usuario y contraseña no pueden ser iguales")
+    elif edad == 18 or edad > 18:
     opcion_1 = input ("\n Tu cuenta sera para adultos o empresarial?: ")
     opcion_1 = opcion_1.lower()
     if opcion_1 == 'adultos':
